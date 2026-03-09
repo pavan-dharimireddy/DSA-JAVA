042b92f5a85335ec64bdeefe0b76ac5d52f33d83
Pavan Dharimireddy <pavandharimireddy.999@gmail.com>
2026-03-09 23:33:05 +0530

    LeetCode 485. Max Consecutive Ones

---
+++ Array/LeetCode_485_Max_Consecutive_Ones.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        int n = nums.length;
+        int max_count = Integer.MIN_VALUE;
+        int count = 0;
+        for(int x:nums){
+            if(x==1){
+                count++;
+            }
+            else{
+                max_count = Math.max(max_count,count);
+                count = 0;
+            }
+        }
+        max_count = Math.max(max_count,count);
+
+        return max_count;
+
+        
+    }
+}
