e32f316929eb46398636cf9217b88cb4da88bd4b
Pavan Dharimireddy <pavandharimireddy.999@gmail.com>
2026-05-22 23:47:50 +0530

    LeetCode_104_Maximum Depth of Binary Tree readme

--- Binary_Tree/LeetCode_104_Maximum Depth of Binary Tree.java
+++ Binary_Tree/LeetCode_104_Maximum Depth of Binary Tree.java
@@ -1,30 +1,33 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 
 /*
-Time Complexity: O(N), each node is processed once in Level Order Traversal.
-Space Complexity: O(N), in worst case, a maximum of N/2 nodes can be present in queue.
-
-*/
+ * Time Complexity: O(N), each node is visited exactly once.
+ * Space Complexity: O(H), where H is the height of the tree. This accounts for
+ * the space used by the recursive call stack.
+ * In the worst case (skewed tree), H = N, leading to O(N) space.
+ * In the best/average case (balanced tree), H = log(N), leading to O(log N)
+ * space.
+ */
 
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null){
+        if (root == null) {
             return 0;
         }
-        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 }
