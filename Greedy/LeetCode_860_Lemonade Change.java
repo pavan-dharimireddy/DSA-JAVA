51be12bf6009f7933e5cc398769f173d35b84dbb
Pavan Dharimireddy <pavandharimireddy.999@gmail.com>
2026-06-02 21:29:23 +0530

    LeetCode_860_Lemonade Change.java

---
+++ Greedy/LeetCode_860_Lemonade Change.java
@@ -0,0 +1,36 @@
+/*
+T.C -- O(N)
+S.C -- O(1)
+*/
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+
+        for (int i : bills) {
+            if (i == 5) {
+                five++;
+            } 
+            else if (i == 10) {
+                if (five > 0) {
+                    five--;
+                    ten++;
+                } else {
+                    return false;
+                }
+            } 
+            else { // i == 20
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } 
+                else if (five >= 3) {
+                    five -= 3;
+                } 
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
